4d1f2e06dac7fd96388f2e0de8819e5846fe6a43
Evan Wright <evanpw@gmail.com>
2023-05-07 17:16:02 -0400

    put build products in a separate directory

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-nasm -f elf64 -g -F dwarf boot.asm -o boot.o
-$HOME/opt/cross/bin/x86_64-elf-ld -Ttext=0x7c00 -melf_x86_64 -e0x7c00  boot.o -o boot.elf
-$HOME/opt/cross/bin/x86_64-elf-objcopy -O binary boot.elf boot.bin
+mkdir -p build
+nasm -f elf64 -g -F dwarf boot.asm -o build/boot.o
+$HOME/opt/cross/bin/x86_64-elf-ld -Ttext=0x7c00 -melf_x86_64 -e0x7c00 build/boot.o -o build/boot.elf
+$HOME/opt/cross/bin/x86_64-elf-objcopy -O binary build/boot.elf build/boot.bin
