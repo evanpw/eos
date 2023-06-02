c2ef1e4fd92c4f7dfec9cb30f63e8a6930045526
Evan Wright <evanpw@gmail.com>
2023-06-01 21:58:05 -0400

    Add a ring buffer for keyboard events

---
+++ ring_buffer.h
@@ -0,0 +1,54 @@
+#pragma once
+#include "print.h"
+
+template <typename T, size_t N>
+class RingBuffer
+{
+public:
+    RingBuffer() : _head(&_data[0]), _tail(&_data[0]) {}
+
+    void push(const T& value) {
+        // If at max capacity, drop from the head
+        if (size() == N) {
+            ++_head;
+        }
+
+        *_tail = value;
+        _tail = increment(_tail);
+    }
+
+    T pop() {
+        ASSERT(size() != 0);
+        T result = *_head;
+        _head = increment(_head);
+        return result;
+    }
+
+    const T& peek() const {
+        ASSERT(size() != 0);
+        return *_head;
+    }
+
+    size_t size() const {
+        if (_tail >= _head) {
+            return _tail - _head;
+        } else {
+            return (_tail + N + 1) - _head;
+        }
+    }
+
+private:
+    T* increment(T* ptr) {
+        ++ptr;
+
+        if (ptr == &_data[N + 1]) {
+            ptr = &_data[0];
+        }
+
+        return ptr;
+    }
+
+    T _data[N + 1];
+    T* _head;
+    T* _tail;
+};
