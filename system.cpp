b42842a73ff474a476e5404271435f0252760e91
Evan Wright <evanpw@gmail.com>
2023-06-11 15:59:19 -0400

    Allow six arguments to a syscall

--- system.cpp
+++ system.cpp
@@ -29,7 +29,7 @@ static void switchAddressSpace(PhysicalAddress pml4) {
 }
 
 using SyscallHandler = int64_t (*)(uint64_t, uint64_t, uint64_t, uint64_t,
-                                   uint64_t);
+                                   uint64_t, uint64_t);
 
 int64_t sys_add(int64_t a, int64_t b) { return a + b; }
 
@@ -38,7 +38,11 @@ int64_t sys_print(int64_t arg) {
     return 0;
 }
 
-static constexpr uint64_t MAX_SYSCALL_NO = 1;
+int64_t sys_add6(int64_t arg1, int64_t arg2, int64_t arg3, int64_t arg4, int64_t arg5, int64_t arg6) {
+    return arg1 + arg2 + arg3 + arg4 + arg5 + arg6;
+}
+
+static constexpr uint64_t MAX_SYSCALL_NO = 2;
 
 extern "C" SyscallHandler syscallTable[];
 
@@ -113,6 +117,7 @@ void System::run() {
     // Create table of syscall handlers
     syscallTable[0] = reinterpret_cast<SyscallHandler>(sys_add);
     syscallTable[1] = reinterpret_cast<SyscallHandler>(sys_print);
+    syscallTable[2] = reinterpret_cast<SyscallHandler>(sys_add6);
 
     println("Entering ring3");
 
--- user.cpp
+++ user.cpp
@@ -2,33 +2,25 @@
 
 #include <stdint.h>
 
-// syscall calling convention: rax (syscall #), rdi, rsi, rdx, r10, r8
+// syscall calling convention: rax (syscall #), rdi, rsi, rdx, r10, r8, r9
 
 int64_t syscall(uint64_t function, uint64_t arg1 = 0, uint64_t arg2 = 0,
-                uint64_t arg3 = 0, uint64_t arg4 = 0, uint64_t arg5 = 0) {
+                uint64_t arg3 = 0, uint64_t arg4 = 0, uint64_t arg5 = 0, uint64_t arg6 = 0) {
     int64_t result;
 
     register uint64_t r10 __asm__("r10") = arg4;
     register uint64_t r8 __asm__("r8") = arg5;
+    register uint64_t r9 __asm__("r9") = arg6;
     asm volatile("syscall"
                  : "=a"(result)
-                 : "a"(function), "D"(arg1), "S"(arg2), "d"(arg3), "r"(r10),
-                   "r"(r8)
+                 : "a"(function), "D"(arg1), "S"(arg2), "d"(arg3), "r"(r10), "r"(r8), "r"(r9)
                  : "rcx", "r11", "memory");
 
     return result;
 }
 
 void umain() {
-    int64_t r1 = syscall(0, 1, 2);
-    syscall(1, r1);
-    int64_t r2 = syscall(0, 3, 4);
-    syscall(1, r2);
-    int64_t r3 = syscall(0, 5, 6);
-    syscall(1, r3);
-    int64_t r4 = syscall(0, r1, r2);
-    syscall(1, r4);
-    int64_t result = syscall(0, r4, r3);
+    int64_t result = syscall(2, 1, 2, 3, 4, 5, 6);
     syscall(1, result);
 
     while (true)
