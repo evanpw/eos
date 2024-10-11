ed69320e913fb300dbb8ee6a3f67dd76932881d8
Evan Wright <evanpw@gmail.com>
2024-10-11 07:48:21 -0400

    get less ready for pipes

--- user/less.cpp
+++ user/less.cpp
@@ -7,13 +7,13 @@
 
 static constexpr size_t BUFFER_SIZE = 4096;
 
-char getchar() {
+char waitForKey(int tty) {
     char c;
-    read(STDIN_FILENO, &c, 1);
+    read(tty, &c, 1);
     return c;
 }
 
-int doLess(int fd) {
+int doLess(int fd, int tty) {
     char* buffer = new char[BUFFER_SIZE];
 
     bool screenDirty = false;  // we've written something since the last clear
@@ -55,7 +55,7 @@ int doLess(int fd) {
             // to continue
             if (currentRow == 24) {
                 print("\033[30;47m-- More --\033[m");
-                getchar();
+                waitForKey(tty);
 
                 // Clear the screen and continue with the next line
                 print("\033[2J");
@@ -69,40 +69,51 @@ int doLess(int fd) {
 
     if (screenDirty) {
         print("\033[30;47m-- END --\033[m");
-        getchar();
+        waitForKey(tty);
     }
 
     return 0;
 }
 
 int main(int argc, char* argv[]) {
-    /*
-    int fd = open("/dev/tty", 0);
-    if (fd < 0) {
-        println("error opening /dev/tty: {}", errno);
-        return 1;
-    }
-
-    write(fd, "Hello, world!\n", 14);
-    close(fd);
-    */
+    int fd, tty;
 
     if (argc < 2) {
-        // TODO: print to stderr
-        println("Usage: {} <file>", argv[0]);
-        return 1;
+        // No filename given, read from stdin
+
+        // If stdin is a terminal, then we don't have any input
+        if (isatty(STDIN_FILENO)) {
+            println("Usage: {} <file>", argv[0]);
+            return 1;
+        }
+
+        fd = STDIN_FILENO;
+
+        // If input comes from stdin, we need a different fd to get keyboard input
+        tty = open("/dev/tty", 0);
+        if (tty < 0) {
+            println("{}: cannot open /dev/tty: {}", argv[0], errno);
+            return 1;
+        }
+    } else {
+        // Filename given on the command line, read from that
+
+        fd = open(argv[1], 0);
+        if (fd < 0) {
+            println("{}: no such file or directory: {}", argv[0], argv[1]);
+            return 1;
+        }
+
+        tty = STDIN_FILENO;
     }
 
-    int fd = open(argv[1], 0);
-    if (fd < 0) {
-        println("{}: no such file or directory: {}", argv[0], argv[1]);
-        return 1;
-    }
+    println("fd={}, tty={}", fd, tty);
+    sleep(500);
 
     // Switch to alternate terminal mode
     print("\033[?1049h");
 
-    int result = doLess(fd);
+    int result = doLess(fd, tty);
 
     // Switch back to normal terminal mode
     print("\033[?1049l");
